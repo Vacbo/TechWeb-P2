3b22966417a30d4b7dc1dee976fef0af26f85dac
Vacbo <peportella11@gmail.com>
2023-05-08 01:47:53 -0300

    detect enter key (missing axios post to back-end)

--- gamerel/src/App.tsx
+++ gamerel/src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import {useState} from 'react';
 import logo from './controller.svg';
+import axios from "axios";
 import './App.css';
+import { TextField } from '@mui/material';
 
 function App() {
+  const [game, setGame] = useState('');
+  
+  const handleKeyDown = (event: any) => {
+    if (event.key === 'Enter') {
+      // axios post
+      console.log(game);
+    }
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +24,7 @@ function App() {
           <p className="App-logo-slogan"> A game relevance search manger</p>
         </div>
       </header>
+      <TextField id="game" variant="outlined" placeholder="Search or enter game" onChange={event => setGame(event.target.value)} onKeyDown={handleKeyDown}/>
     </div>
   );
 }
