d5548240191599d0fdc7a730ea9725be724904a6
Peng1104 <admin@peng1104.net>
2023-05-10 16:32:06 -0300

    Updated Backend

--- gamerel_back/models.py
+++ gamerel_back/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from rest_framework import serializers
 
-# Create your models here.
+class Game(models.Model):
+
+    gameName = models.TextField()
+    twich_views = models.IntegerField()
+    playtime = models.IntegerField()
+
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ['gameName', 'twitch_views', 'playtime']
--- gamerel_back/views.py
+++ gamerel_back/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import HttpResponse, HttpRequest, Http404
+from models import Game, GameSerializer
+from urllib import parse
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    raise Http404("Not implemented")
+
+@api_view(['GET', 'POST'])
+def api_game(request: HttpRequest, gameName : str) -> HttpResponse:
+    try :
+        parse.quote("Banana".lower())
+        game = Game.objects.get(gameName=gameName)
+    except Game.DoesNotExist:
+        raise Http404("Game does not exist")
+    
+    if request.method == 'POST':
+        # Get gamename and add it to database and search list
+        # get_game(gameName).update(request.data)
+        pass
+    
+    return Response(GameSerializer(game).data)
